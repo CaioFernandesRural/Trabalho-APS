b8f14ed9ddec77263939a9d4ca9a5768ee2e3104
Cassia <cassiamariane01@gmail.com>
2023-01-29 18:45:23 -0300

    telaHome

--- codigo/src/views/index.php
+++ codigo/src/views/index.php
@@ -1,13 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <title>Document</title>
+</head>
 <?php
-
-require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-
-$uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-);
-
-if($uri === '/' || $uri === '' || $uri === '/index.php'){
-    $uri = '/day_records.php';
-}
-
-require_once(CONTROLLER_PATH . "/{$uri}");
+    require_once "header.php";
+?>
+<body>
+    <div id="conteudo">
+        <div id="filtro">
+            <div id="titulo_filtro">
+                <p>Filtros</p>
+            </div>
+            <form id="form_filtro">
+                <input type="checkbox" id="check_1">
+                <label for="check_1">
+                    Checkbox 1
+                </label><br>
+                <input type="checkbox">
+                <label for="check_1">
+                    Checkbox 2
+                </label><br>
+                <input type="checkbox">
+                <label for="check_1">
+                    Checkbox 3
+                </label><br>
+                <input type="checkbox">
+                <label for="check_1">
+                    Checkbox 4
+                </label><br>
+                <input type="checkbox">
+                <label for="check_1">
+                    Checkbox 5
+                </label><br>
+                <input type="checkbox">
+                <label for="check_1">
+                    Checkbox 6
+                </label><br>
+                <input type="checkbox">
+                <label for="check_1">
+                    Checkbox 7
+                </label><br>
+                <input type="checkbox">
+                <label for="check_1">
+                    Checkbox 8
+                </label><br>
+                <input type="checkbox">
+                <label for="check_1">
+                    Checkbox 9
+                </label><br>
+            </form>
+        </div>
+        <div id="cards_eventos">
+            <div id="evento">
+                <a href=""><p id="nome_evento">BSD Day</p></a>
+                <p id="nome_cadastrador">RIZZO, Nilton José;</p>
+                <p id="descricao_evento">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tortor velit, tempor at felis sit amet, pretium maximus augue. In fermentum, ligula ac pharetra</p>
+            </div>
+            <div id="evento">
+                <a href=""><p id="nome_evento">Nome do Evento</p></a>
+                <p id="nome_cadastrador">Nome do Cadastrador;</p>
+                <p id="descricao_evento">(Descrição)</p>
+            </div>
+        </div>
+    </div>
+</body>
+<?php
+    require_once "footer.php";
+?>
+</html>
