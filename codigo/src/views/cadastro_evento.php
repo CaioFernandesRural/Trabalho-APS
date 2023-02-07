09f118814ef4c52a44a266799b47bee5a5d195c4
Cassia <cassiamariane01@gmail.com>
2023-02-06 22:36:10 -0300

    Telas de Cadastro

---
+++ codigo/src/views/cadastro_evento.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../../public/assets/css/style.css"/>
+    <title>Alexandria</title>
+</head>
+<?php
+    require_once "./template/header.php";
+?>
+<body>
+    <p class="titulo_cadastro">Dados do Evento</p>
+    <div class="cadastro">
+        <form>
+            <div class="form_container">
+                <div class="div_form">
+                    <label for="nome_evento">Nome</label>
+                    <input type="text" name="nome_evento" id="nome_evento">
+                    <br>
+                    <label for="edicao">Edição</label>
+                    <input type="text" name="edicao" id="edicao">
+                    <br>
+                    <label for="link_evento">Link do Evento</label>
+                    <input type="url" id="link_evento" name="link_evento">
+                    <br>
+                    <label for="tipo">Tipo</label>
+                    <select name="tipo" id="tipo">
+                        <option value="tipo_1">Tipo 1</option>
+                        <option value="tipo_2">Tipo 2</option>
+                    </select>
+                </div>
+                <div class="div_form">
+                    <label for="email_resp">E-mail do Responsável</label>
+                    <input type="email" id="email_resp" name="email_resp">
+                    <br>
+                    <label for="area">Área de Estudo</label>
+                    <input type="text" id="area" name="area">
+                    <br>
+                    <label for="descricao_evento">Descrição Sucinta</label>
+                    <input type="text" id="descricao_evento" name="descricao_evento">
+                </div>    
+            </div>
+        </form>
+    </div>    
+    <button class="btn_cancela">Cancelar</button>
+    <button class="btn_salva">Enviar</button>
+</body>
+<?php
+    require_once "./template/footer.php";
+?>
+</html>
--- codigo/src/views/home.php
+++ codigo/src/views/home.php
@@ -18,70 +18,70 @@
                 <p>Filtros</p>
             </div>
             <form id="form_filtro">
-                <input type="checkbox" id="check_1">
-                <label for="check_1">
+                <input type="checkbox" id="congresso" name="congresso">
+                <label for="congresso">
                     Congresso
                 </label><br>
-                <input type="checkbox" id="check_2">
-                <label for="check_2">
+                <input type="checkbox" id="simposio" name="simposio">
+                <label for="simposio">
                     Simpósio
                 </label><br>
-                <input type="checkbox" id="check_3">
-                <label for="check_3">
+                <input type="checkbox" id="seminario" name="seminario">
+                <label for="seminario">
                     Seminário
                 </label><br>
-                <input type="checkbox" id="check_4">
-                <label for="check_4">
+                <input type="checkbox" id="mesa" name="mesa">
+                <label for="mesa">
                     Mesa-redonda
                 </label><br>
-                <input type="checkbox" id="check_5">
-                <label for="check_5">
+                <input type="checkbox" id="encontro" name="encontro">
+                <label for="encontro">
                     Encontro
                 </label><br>
-                <input type="checkbox" id="check_6">
-                <label for="check_6">
+                <input type="checkbox" id="painel" name="painel">
+                <label for="painel">
                     Painel
                 </label><br>
-                <input type="checkbox" id="check_7">
-                <label for="check_7">
+                <input type="checkbox" id="forum" name="forum">
+                <label for="forum">
                     Fórum
                 </label><br>
-                <input type="checkbox" id="check_8">
-                <label for="check_8">
+                <input type="checkbox" id="conferencia" name="conferencia">
+                <label for="conferencia">
                     Conferência
                 </label><br>
-                <input type="checkbox" id="check_9">
-                <label for="check_9">
+                <input type="checkbox" id="jornada" name="jornada">
+                <label for="jornada">
                     Jornada
                 </label><br>
-                <input type="checkbox" id="check_10">
-                <label for="check_10">
+                <input type="checkbox" id="workshop" name="workshop">
+                <label for="workshop">
                     Workshop
                 </label><br>
-                <input type="checkbox" id="check_11">
-                <label for="check_11">
+                <input type="checkbox" id="coloquio" name="coloquio">
+                <label for="coloquio">
                     Colóquio
                 </label><br>
-                <input type="checkbox" id="check_12">
-                <label for="check_12">
+                <input type="checkbox" id="cursos" name="cursos">
+                <label for="cursos">
                     Cursos
                 </label><br>
-                <input type="checkbox" id="check_13">
-                <label for="check_13">
+                <input type="checkbox" id="semana" name="semana">
+                <label for="semana">
                     Semana
                 </label><br>
             </form>
         </div>
-        <div id="cards_eventos">
-            <div id="evento">
-                <a href=""><p id="nome_evento">BSD Day</p></a>
-                <p id="nome_cadastrador">RIZZO, Nilton José;</p>
-                <p id="descricao_evento">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tortor velit, tempor at felis sit amet, pretium maximus augue. In fermentum, ligula ac pharetra</p>
+        <div class="cards_eventos">
+            <div class="evento">
+                <a href=""><p class="nome_evento">BSD Day</p></a>
+                <p class="nome_cadastrador">RIZZO, Nilton José;</p>
+                <p class="descricao_evento">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tortor velit, tempor at felis sit amet, pretium maximus augue. In fermentum, ligula ac pharetra</p>
             </div>
-            <div id="evento">
-                <a href=""><p id="nome_evento">Nome do Evento</p></a>
-                <p id="nome_cadastrador">Nome do Cadastrador;</p>
-                <p id="descricao_evento">(Descrição)</p>
+            <div class="evento">
+                <a href=""><p class="nome_evento">Nome do Evento</p></a>
+                <p class="nome_cadastrador">Nome do Cadastrador;</p>
+                <p class="descricao_evento">(Descrição)</p>
             </div>
         </div>
     </div>
