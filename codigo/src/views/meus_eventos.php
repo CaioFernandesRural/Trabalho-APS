b3beb1a2a9251fef0012c1811f6c9e1e7378de1d
caio <caioorebeo@gmail.com>
2023-02-14 21:11:58 -0300

    rotas consertadas

--- codigo/src/views/meus_eventos.php
+++ codigo/src/views/meus_eventos.php
@@ -4,13 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../../../public/assets/css/style.css"/>
-    <link rel="stylesheet" type="text/css" href="../../../public/assets/css/meus_eventos.css"/>
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="../assets/css/meus_eventos.css"/>
     <title>Alexandria</title>
 </head>
-<?php
-    require_once "../template/header.php";
-?>
 <body>
     <div class="container">
 
@@ -51,7 +48,4 @@
     </div>
 </div>
 </body>
-<?php
-    require_once "../template/footer.php";
-    ?>
 </html>
--- codigo/src/views/template/header.php
+++ codigo/src/views/template/header.php
@@ -1,14 +1,14 @@
 <header class="container">
-    <a href="../usuariocomum/home.php" class="logo">
+    <a href="/home.php" class="logo">
         <div>
             <h3>Alexandria</h3>
         </div>
     </a>
     <label id="pesquisa">
         <input type="text" id="barra_pesquisa" placeholder="Buscar">
-        <img src="../../../public/assets/img/icon_lupa.png" id="icon_lupa">
+        <img src="../assets/img/icon_lupa.png" id="icon_lupa">
     </label>
-    <img src="../../../public/assets/img/icon_person.png" id="icon_person">
+    <img src="../assets/img/icon_person.png" id="icon_person">
     <!-- Logado
     <img src="../../public/assets/img/logout.png" id="icon_logout">
 -->
