e88b0ddbcfe79ed131bf218bed61fcf073d726fa
caio <caioorebeo@gmail.com>
2023-02-28 21:00:27 -0300

    corrigi 1 botão no cadastro_usuario

--- codigo/src/views/cadastro_usuario.php
+++ codigo/src/views/cadastro_usuario.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
-    <link rel="stylesheet" type="text/css" href="assets/css/cadastro_usuario.css"/>
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="/assets/css/cadastro_usuario.css"/>
     <title>Alexandria</title>
 </head>
 
@@ -88,11 +88,12 @@
                     </div>
                 </div>    
             </div>
+            <div class="button-container">
+                <button class="btn_cancela">Cancelar</button>
+                <button class="btn_salva">Enviar</button>
+            </div>
         </form>
-        <div class="button-container">
-            <button class="btn_cancela">Cancelar</button>
-            <button class="btn_salva">Enviar</button>
-        </div>
+        
     </div>
 </div>
 </body>
